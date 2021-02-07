d628c9bccc1ff60044e8c29b267d9b6b6c52fd0d
Talendar <51515680+Talendar@users.noreply.github.com>
2021-02-06 23:28:26 -0300

    Update version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@
 from typing import List
 import setuptools
 
-_VERSION = "0.0.1"
+_VERSION = "0.1.0"
 
 # Short description.
 short_description = "An OpenAI gym environment for the Flappy Bird game."
